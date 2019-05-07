7c9a243f47e0f4515d8356e7e45a49501b2536ca
igor <sas.ihor.new@gmail.com>
2019-05-07 15:36:57 +0300

    added tasks

---
+++ Graphics/Graphics/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.IsSoftShadows = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(795, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 318);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 346);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(62, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(57, 380);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(62, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(442, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 318);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 349);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "step:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "y = sin(x)",
+            "y = cos(x)"});
+            this.comboBox1.Location = new System.Drawing.Point(208, 318);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Graphics/Graphics/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Graphics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Bitmap bitmap = new Bitmap(chart1.Width, chart1.Height);
+            //Rectangle rectangle = new Rectangle(0, 0, chart1.Width, chart1.Height);
+            //chart1.DrawToBitmap(bitmap, rectangle);
+            //pictureBox1.Image = bitmap;
+
+        }
+
+        private void buttonCalc_Click(object sender,EventArgs e)
+        {
+            if (comboBox1.SelectedItem == null) {
+                MessageBox.Show("Please, enter function from comboBox");
+                return;
+            }
+            chart1.Series[0].Name = comboBox1.SelectedItem.ToString();
+            try
+            {
+                bool sin = comboBox1.SelectedItem.ToString().Equals("y = sin(x)");
+                double Xmin = double.Parse(textBox1.Text);
+                double Xmax = double.Parse(textBox2.Text);
+                double Step = double.Parse(textBox3.Text);
+            
+            if(Xmin >= Xmax)
+            {
+                MessageBox.Show("A should be bigger then B");
+                return;
+            }
+
+            if(Step > Xmax - Xmin || Step <= 0)
+            {
+                MessageBox.Show("Please, enter correct step");
+                return;
+            }
+
+            int count = (int)Math.Ceiling((Xmax - Xmin) / Step)+ 1;
+            double[] x = new double[count];
+            double[] y1 = new double[count];
+
+            double[] y2 = new double[count];
+            for (int i = 0; i < count; i++)
+
+            {
+                x[i] = Xmin + Step * i;
+                if (sin)
+                {
+                    y1[i] = Math.Sin(x[i]);
+                }
+                else
+                {
+                    y1[i] = Math.Cos(x[i]);
+                }
+
+            }
+            
+            chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+
+            chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+            
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step;
+            
+            chart1.Series[0].Points.DataBindXY(x, y1);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Incorrect data!");
+            }
+        }
+    }
+}
